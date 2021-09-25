7f560f045e8c06451a2db1b85dd9bc13cb4346f0
iobaidu5 <i.obaidu5@gmail.com>
2021-09-25 16:10:27 +0500

    templete files added

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-const http = require('http');
+const path = require('path');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const server = http.createServer(app);
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-server.listen(3000);
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
+
+app.listen(3000);
